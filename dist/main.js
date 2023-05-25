d98f21066095e6153fc7af37fedcc1658525be40
BTN5211\edyta.szarowska <edyta.szarowska@interia.pl>
2023-05-25 07:22:58 +0200

    back to previous settings

--- dist/main.js
+++ dist/main.js
@@ -73,7 +73,7 @@ var SDK = __webpack_require__(19);
 var sdk = new SDK(null, null, true); // 3rd argument true bypassing https requirement: not prod worthy
 
 var address, width, height, zoom, link, 
-// mapsKey
+mapsKey
 ;
 
 function debounce (func, wait, immediate) {
@@ -92,7 +92,7 @@ function debounce (func, wait, immediate) {
 }
 
 function paintSettings () {
-	// document.getElementById('text-input-id-0').value = mapsKey;
+	document.getElementById('text-input-id-0').value = mapsKey;
 	document.getElementById('text-input-id-1').value = address;
 	document.getElementById('slider-id-01').value = width;
 	document.getElementById('slider-id-02').value = height;
@@ -107,7 +107,7 @@ function paintSliderValues () {
 
 function paintMap() {
 	// mapsKey='AIzaSyAJP73JRaOwhLZV7g1Sbwn34X7CRKejSNA'
-	// mapsKey = document.getElementById('text-input-id-0').value;
+	mapsKey = document.getElementById('text-input-id-0').value;
 	address = document.getElementById('text-input-id-1').value;
 	width = document.getElementById('slider-id-01').value;
 	height = document.getElementById('slider-id-02').value;
@@ -118,8 +118,8 @@ function paintMap() {
 	}
 	var url = 'https://maps.googleapis.com/maps/api/staticmap?center=' +
 		address.split(' ').join('+') + '&size=' + width + 'x' + height + '&zoom=' + zoom +
-		'&markers=' + address.split(' ').join('+') + '&key=AIzaSyAJP73JRaOwhLZV7g1Sbwn34X7CRKejSNA'
-		//  + mapsKey
+		'&markers=' + address.split(' ').join('+') + '&key='
+		 + mapsKey
 		 ;
 	sdk.setContent('<a href="' + link + '"><img src="' + url + '" /></a>');
 	sdk.setData({
@@ -128,9 +128,9 @@ function paintMap() {
 		height: height,
 		zoom: zoom,
 		link: link,
-		// mapsKey: mapsKey
+		mapsKey: mapsKey
 	});
-	// localStorage.setItem('googlemapsapikeyforblock', mapsKey);
+	localStorage.setItem('googlemapsapikeyforblock', mapsKey);
 }
 
 sdk.getData(function (data) {
@@ -139,7 +139,7 @@ sdk.getData(function (data) {
 	height = data.height || 300;
 	zoom = data.zoom || 15;
 	link = data.link || '';
-	// mapsKey = data.mapsKey || localStorage.getItem('googlemapsapikeyforblock');
+	mapsKey = data.mapsKey || localStorage.getItem('googlemapsapikeyforblock');
 	paintSettings();
 	paintSliderValues();
 	paintMap();
